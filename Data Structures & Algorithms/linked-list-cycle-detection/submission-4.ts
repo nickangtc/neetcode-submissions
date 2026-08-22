86f488983e701dd9fbc5f2e93144df0ae6105f53
Nick Ang <nickangtc@gmail.com>
2026-08-22 16:01:13 +0200

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        // assumption I'm going to make:
+        // every node has a unique value.
+        const seen = new Map<ListNode, boolean>()
+
+        while (head) {
+            if (seen.has(head)) {
+                return true
+            }
+            seen.set(head, true)
+            head = head.next
+        }
+        return false
+    }
+}
+
+
+
+
+
+
+
+
