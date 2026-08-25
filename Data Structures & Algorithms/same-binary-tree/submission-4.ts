6ba11d2ad63ffa39c59bc7aea62d030f1443135f
Nick Ang <nickangtc@gmail.com>
2026-08-25 13:24:31 +0200

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        function dfs(node): number[] {
+            if (node === null) {
+                return [null]
+            }
+
+            const leftArray = dfs(node.left)
+            const rightArray = dfs(node.right)
+
+            return [...leftArray, ...rightArray, node.val]
+        }
+
+        const pa = dfs(p)
+        const qa = dfs(q)
+
+        return pa.every((x, i) => qa[i] === x) && qa.every((x, i) => pa[i] === x)
+    }
+}
