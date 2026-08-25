6d0db5bd5dc4e387f611e7f349c2535a3153a27f
Nick Ang <nickangtc@gmail.com>
2026-08-25 10:36:39 +0200

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.ts
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1: string, s2: string): boolean {
+        const d1 = s1.split('').reduce((a,v) => {
+            return {
+                ...a,
+                [v]: (a[v] ?? 0) + 1
+            }
+        } ,{}) // O(n) space, O(n) time
+
+        let d1copy = {...d1} // O(n) space
+        let left = 0
+        let right = 0
+
+        // if valid, right++
+        // if invalid, left++, right = left
+        while (left < s2.length) { // O(n) time
+            const rightChar = s2.charAt(right)
+
+            if (d1copy[rightChar]) {
+                d1copy[rightChar]--
+                if (d1copy[rightChar] === 0) {
+                    delete d1copy[rightChar]
+                }
+
+                if (Object.keys(d1copy).length === 0) { // O(n) space
+                    return true
+                }
+
+                right++
+            } else {
+                d1copy = {...d1}
+                left++
+                right = left
+            }
+        }
+        return false
+    }
+}
+
+
+
+
+
+
+
+
+
