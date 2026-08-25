6bcd1e4bf8a6209756b2ba3fcd6c1c437d812114
Nick Ang <nickangtc@gmail.com>
2026-08-25 14:00:10 +0200

    Add: subtree-of-a-binary-tree - submission-5

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-5.ts
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     * 
+     * time complexity: O(n * m) = O(n)
+     * space complexity: O(n)
+     */
+    isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+        // stack of subtrees to check against one by one
+        // isIdentical(t1, t2): boolean
+        if (!root || !subRoot) {
+            return false
+        }
+
+        function isIdentical(t1: TreeNode, t2: TreeNode): boolean {
+            if (!t1 || !t2) {
+                return t1 === t2
+            }
+
+            // val check on t1 and t2 is shortcircuiting to false
+            // and won't cause DFS to finish, exiting early.
+            // => O(m) time - max is touching every node in subRoot
+            // => O(h) space - stack space according to height
+            return t1.val === t2.val &&
+                isIdentical(t1.left, t2.left) &&
+                isIdentical(t1.right, t2.right)
+        }
+
+        // O(n) space -> max is total number of nodes in root
+        // O(n) time -> max is total number of nodes in root
+        const subtrees = [root]
+
+        while (subtrees.length > 0) {
+            const t = subtrees.pop()
+
+            if (isIdentical(t, subRoot)) {
+                return true
+            }
+
+            if (t.left) {
+                subtrees.push(t.left)
+            }
+            if (t.right) {
+                subtrees.push(t.right)
+            }
+        }
+
+        return false
+    }
+}
