72872ad0ebc7e6e0c3116f4abfb2a330584dd597
Nick Ang <nickangtc@gmail.com>
2026-09-07 13:29:39 +0200

    Add: combination-target-sum-ii - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-6.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates: number[], target: number): number[][] {
+        // remember: sort in JS does inplace, mutations.
+        candidates.sort((a,b) => a - b)
+
+        const res = []
+        const subset = []
+
+        function dfs(i: number, sum: number): void {
+            if (sum === target) {
+                res.push([...subset])
+                return
+            } 
+            if (sum > target || i >= candidates.length) {
+                return
+            }
+
+            // left decision: take canddiate, even if it's same number as previous
+            subset.push(candidates[i])
+            dfs(i+1, sum + candidates[i])
+            subset.pop()
+
+            // right decision: take candidate only if it's NOT same as previous
+            while (candidates[i] === candidates[i+1] && i + 1 < candidates.length) {
+                i++
+            }
+            dfs(i + 1, sum)
+        }
+
+        dfs(0, 0)
+        return res
+    }
+}
