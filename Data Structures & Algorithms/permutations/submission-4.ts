cda327f2beaf10bef0e64d3f48ea0905ca7e9e9d
Nick Ang <nickangtc@gmail.com>
2026-09-07 14:47:56 +0200

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums: number[]): number[][] {
+        let perms = [[]]
+
+        for (const num of nums) {
+            // e.g. num = 1
+            const newPerms = []
+            for (const perm of perms) {
+                console.log('num', num)
+                // e.g. perm = [1,2] ... [2] ... [3,4,5]
+                for (let i = 0; i < perm.length + 1; i++) {
+                    const permCopy = [...perm]
+                    permCopy.splice(i,0,num)
+                    newPerms.push(permCopy)
+                }
+            }
+            perms = newPerms
+        }
+        return perms
+    }
+}
